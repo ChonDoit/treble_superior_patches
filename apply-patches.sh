1509c3a4efbaa6afe04b28b5ce6d8e083164ab37
ChonDoit <thphantomblog@gmail.com>
2024-04-01 11:37:44 -0300

    v20240401

--- apply-patches.sh
+++ apply-patches.sh
@@ -6,6 +6,7 @@ source="$(readlink -f -- $1)"
 core="$source/patches/trebledroid"
 personal="$source/patches/personal"
 pre="$source/patches/pre"
+fod="$source/patches/fod"
 
 if [ -e $pre ]; then
     printf "\n ##### APPLYING PRE-REQUISITE PATCHES #####\n";
@@ -114,5 +115,40 @@ if [ -e $personal ]; then
     printf "\n##### THERE'S NO PERSONAL PATCHES#####\n";
 fi
 
-rm -rf $1/patches
-rm $1/patches.zip
+if [ -e $fod ]; then
+    printf "\n##### APPLYING FOD PATCHES #####\n";
+    sleep 1.0;
+    for path_fod in $(cd $fod; echo *); do
+    	tree="$(tr _ / <<<$path_fod | sed -e 's;platform/;;g')"
+    	printf "\n| $path_fod #####\n";
+    	[ "$tree" == build ] && tree=build/make
+        [ "$tree" == vendor/hardware/overlay ] && tree=vendor/hardware_overlay
+        [ "$tree" == treble/app ] && tree=treble_app
+    	pushd $tree
+    	
+    	for patch in $fod/$path_fod/*.patch; do
+    		# Check if patch is already applied
+    		if patch -f -p1 --dry-run -R < $patch > /dev/null; then
+                printf "##### ALREDY APPLIED: $patch \n";
+    			continue
+    		fi
+    
+    		if git apply --check $patch; then
+    			git am $patch
+    		elif patch -f -p1 --dry-run < $patch > /dev/null; then
+    			#This will fail
+    			git am $patch || true
+    			patch -f -p1 < $patch
+    			git add -u
+    			git am --continue
+    		else
+    			printf "##### FAILED APPLYING: $patch \n"
+    		fi
+    	done
+    
+    	popd
+    done
+        else
+    printf "\n##### THERE'S NO FOD PATCHES#####\n";
+fi
+
