1d055d3e4bb114d5a67d8734523fa1468ab43a9c
ChonDoit <thphantomblog@gmail.com>
2024-10-02 21:56:49 -0300

    QPR3

--- apply-patches.sh
+++ apply-patches.sh
@@ -116,43 +116,6 @@ if [ -e $fod ]; then
     printf "\n##### THERE'S NO FOD PATCHES#####\n";
 fi
 
-if [ -e $health ]; then
-    printf "\n##### APPLYING HEALTH PATCHES #####\n";
-    sleep 1.0;
-    for path_health in $(cd $health; echo *); do
-    	tree="$(tr _ / <<<$path_health | sed -e 's;platform/;;g')"
-    	printf "\n| $path_health #####\n";
-    	[ "$tree" == build ] && tree=build/make
-        [ "$tree" == vendor/hardware/overlay ] && tree=vendor/hardware_overlay
-        [ "$tree" == treble/app ] && tree=treble_app
-    	pushd $tree
-    	
-    	for patch in $health/$path_health/*.patch; do
-    		# Check if patch is already applied
-    		if patch -f -p1 --dry-run -R < $patch > /dev/null; then
-                printf "##### ALREDY APPLIED: $patch \n";
-    			continue
-    		fi
-    
-    		if git apply --check $patch; then
-    			git am $patch
-    		elif patch -f -p1 --dry-run < $patch > /dev/null; then
-    			#This will fail
-    			git am $patch || true
-    			patch -f -p1 < $patch
-    			git add -u
-    			git am --continue
-    		else
-    			printf "##### FAILED APPLYING: $patch \n"
-    		fi
-    	done
-    
-    	popd
-    done
-        else
-    printf "\n##### THERE'S NO HEALTH PATCHES#####\n";
-fi
-
 if [ -e $personal ]; then
     printf "\n##### APPLYING PERSONAL PATCHES #####\n";
     sleep 1.0;
